e5cbb319265f7e6c8c82426982c77af5871df8bc
Devante Rose <drose2021@yahoo.com>
2023-08-11 02:40:13 -0400

    fixed work more

--- hw3-assignment.py
+++ hw3-assignment.py
@@ -9,37 +9,26 @@
 # 6) Upon quiting the program, print out all items in the user's list -->
 def store():
     shopping_cart = {}
-    while True:  
-        quantity= input("please enter amount of item or Enter quit to quit! and delete to delete! : ")
     
-        if quantity.strip().lower() != 'quit' and 'delete':
-            item = input("please enter item or Enter quit to quit! and delete to delete! : ")
-        shopping_cart[quantity] = item
-         
-
-        if quantity.strip().lower() == 'delete' or item.strip().lower() == 'delete':
-            shopping_cart.popitem()
+    while True:
+        item = input("please enter item or Enter quit to quit! and delete to delete! : ")  
         
-        print(shopping_cart)
+        if item.strip().lower() != 'quit' or 'delete':
+            quantity = input("please enter amount of item or Enter quit to quit! and delete to delete! : ")
         
-        if item.strip().lower() == 'quit' or quantity.strip().lower() == 'quit':
+        elif item.strip().lower() == 'delete' or quantity.strip().lower() == 'delete':
+              delete = input("what item do you want to delete")
+              del(delete)
+        
+        else:
             for item, quantity in shopping_cart.items():
                 print(f"{quantity} {item} is in shopping cart")
             break
-       
+        shopping_cart[item] = quantity
+
+        print(shopping_cart)  
+   
 store()
-        
-
-       
-    
-    
-       
-    
-    
-    
-
-
-
 
 
 # <!-- 2) Set Practice
@@ -49,15 +38,15 @@ nums_list = [1, 1, 1, 2, 2, 3, 5, 6, 4, 12, 11, 12, 12, 14, 16, 16, 16, 1, 1, 1,
 my_list = list(set(nums_list))
 print(my_list)
 # Out put the intersection of the following the following sets.
-# set1 = {20, 24, 26, 27}
-# set2 = {26, 35, 63, 27}
-# set7 = set1.intersection(set2)
-# print(set7)
+set1 = {20, 24, 26, 27}
+set2 = {26, 35, 63, 27}
+set7 = set1.intersection(set2)
+print(set7)
 
 # Output the difference between the following sets
-# set3 = {100, 65, 89, 200}
-# set4 = {65, 103, 54, 200}
-# set5 = set3.difference(set4)
-# set6 = set4.difference(set3)
-# print(set5)
-# print(set6)
+set3 = {100, 65, 89, 200}
+set4 = {65, 103, 54, 200}
+set5 = set3.difference(set4)
+set6 = set4.difference(set3)
+print(set5)
+print(set6)
